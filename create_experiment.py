b7cb8f8bcde3e164834ca7b790d9c7a2bdf2ceb6
Ashutosh <Ashutosh07@users.noreply.github.com>
2024-01-21 15:54:14 +1100

    Updated Util

--- create_experiment.py
+++ create_experiment.py
@@ -18,6 +18,18 @@ tags = {
 First_ML_Experiement = create_experiments("First_ML_Experiemnt", "first_test_flow.txt",tags)
 print(First_ML_Experiement)
 
-#print("New Experiment has been created with ID: %s"%(First_ML_Experiement.experiment_id))
-#print("Experiment name is: {}".format(First_ML_Experiement.name))
-#print("New Experiment has been created with ID: {}".format(First_ML_Experiement.experiment_id))
+print("New Experiment has been created with ID: %s"%First_ML_Experiement)
+print("Experiment name is: {}".format(mlflow.get_experiment(First_ML_Experiement).name))
+print("New Experiment has been created with ID: {}".format(mlflow.get_experiment_by_name(mlflow.get_experiment_(First_ML_Experiement).name)))
+
+experiment = get_mlflow_experiment(experiment_id=First_ML_Experiement)
+with mlflow.start_run(run_name ="First_Run", experiment_id=experiment.experiment_id) as run:
+    # Your machine learning code goes here
+        mlflow.log_param("learning_rate",0.01)
+        # print run info    
+        print("run_id: {}".format(run.info.run_id))
+        print("experiment_id: {}".format(run.info.experiment_id))
+        print("status: {}".format(run.info.status))
+        print("start_time: {}".format(run.info.start_time))
+        print("end_time: {}".format(run.info.end_time))
+        print("lifecycle_stage: {}".format(run.info.lifecycle_stage))
